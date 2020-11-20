49975a469ae15977ce1351a86cfe5925a38ca2e1
LeontyV <47623652+LeontyV@users.noreply.github.com>
2020-11-20 14:50:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 BOT_TOKEN = ""  # Your token
 admin_id =  # Your id
 PROXY = "Socks5://138.197.157.60:1080" #138.197.157.45	1080
-accepted_chats = ['uc_cit_alert']
+accepted_chats = ['']
 uc_chat_id = 
 accepted_users = ['']
